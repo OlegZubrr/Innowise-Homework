2450b9d14698bd7537011842d57f347a27454a51
artem <ghtp7397@gmail.com>
2025-10-06 00:37:09 +0300

    add 1 week

---
+++ Week1/Task1/CalculatorConsoleApp/Program.cs
@@ -0,0 +1,93 @@
+﻿using System.Globalization;
+
+namespace CalculatorConsoleApp;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        
+        bool isProcesing = true;
+
+        while (isProcesing)
+        {
+            double a;
+            double b;
+            char operation;
+            char[] operations = {'+', '-', '*', '/'};
+            double result;
+       
+        
+            Console.Write("Enter the first real number: ");
+            while (!double.TryParse(Console.ReadLine(), NumberStyles.Float, CultureInfo.InvariantCulture, out a))
+            {
+                Console.WriteLine("Incorrect input");
+                Console.Write("Enter the first real number: ");
+            }
+
+            Console.Write("Enter operation (+, -, *, /): ");
+            while (!char.TryParse(Console.ReadLine(), out operation) || !operations.Contains(operation))
+            {
+                Console.WriteLine("Incorrect input");
+                Console.Write("Enter operation (+, -, *, /): ");
+            }
+        
+            Console.Write("Enter the second real number: ");
+            while (!double.TryParse(Console.ReadLine(), NumberStyles.Float, CultureInfo.InvariantCulture, out b))
+            {
+                Console.WriteLine("Incorrect input");
+                Console.Write("Enter the second real number: ");
+            }
+        
+            result = Calculate(a,b,operation);
+            if (double.IsNaN(result) || double.IsInfinity(result))
+            {
+                Console.WriteLine("You can't divide by zero");
+                continue;
+            }
+            Console.WriteLine($"{a} {operation} {b} = {Math.Round(result,3)}");
+
+            Console.Write("Continue ? [Y/n]: ");
+            
+            string? answer = Console.ReadLine();
+            while (!string.IsNullOrEmpty(answer) && answer.ToLower() != "y" && answer.ToLower() != "n")
+            {
+                Console.WriteLine("Incorrect input");
+                Console.Write("Continue ? [Y/n]: ");
+                answer = Console.ReadLine();
+            }
+            
+            isProcesing = answer.ToLower() == "y"  || string.IsNullOrEmpty(answer);
+            
+        }
+      
+    }
+
+    static double Calculate(double a, double b,char operation)
+    {
+        double result = 0;
+
+        switch (operation)
+        {
+            case '+':
+                result = a + b;
+                break;
+            case '-':
+                result = a - b;
+                break;
+            case '*':
+                result = a * b;
+                break;
+            case '/':
+                if (b == 0)
+                {
+                    result =  double.NaN; 
+                    return result;
+                }
+                result =  a / b;
+                break;
+
+        }
+        return result;
+    }
+}
---
+++ Week1/Task2/WorkingWithAsynchrony/Program.cs
@@ -0,0 +1,50 @@
+﻿namespace WorkingWithAsynchrony;
+
+class Program
+{
+    static async Task Main(string[] args)
+    {
+        Console.WriteLine("=== Synchronous processing ===");
+        var watchSync = System.Diagnostics.Stopwatch.StartNew();
+        
+        Console.WriteLine(ProcessData("File 1"));
+        Console.WriteLine(ProcessData("File 2"));
+        Console.WriteLine(ProcessData("File 3"));
+        
+        watchSync.Stop();
+        
+        Console.WriteLine($"Synchronous execution took: {watchSync.ElapsedMilliseconds / 1000.0} sec.\n");
+
+
+        Console.WriteLine("=== Asynchronous processing ===");
+        var watchAsync = System.Diagnostics.Stopwatch.StartNew();
+        
+        Task<string> t1 = ProcessDataAsync("File 1");
+        Task<string> t2 = ProcessDataAsync("File 2");
+        Task<string> t3 = ProcessDataAsync("File 3");
+        
+        
+        var results = await Task.WhenAll(t1, t2, t3);
+        
+        foreach (var result in results)
+        {
+            Console.WriteLine(result);
+        }
+        
+        watchAsync.Stop();
+        Console.WriteLine($"Asynchronous execution took: {watchAsync.ElapsedMilliseconds / 1000.0} sec.\n");
+
+    }
+
+    static string ProcessData(string dataName)
+    {
+        Thread.Sleep(3000);
+        return $"Processing {dataName} completed in 3 seconds";
+    }
+
+    static async Task<string>  ProcessDataAsync(string dataName)
+    {
+        await Task.Delay(3000);
+        return $"Async processing {dataName} completed in 3 seconds";
+    }
+}
